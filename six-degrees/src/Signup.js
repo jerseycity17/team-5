432c3b37ed86c6b9b12c5b08a22459f7ca1b640d
aprado06 <aprado06@users.noreply.github.com>
2017-10-28 05:34:31 -0400

    don't import logo.svg not needed

--- six-degrees/src/Signup.js
+++ six-degrees/src/Signup.js
@@ -1,6 +1,5 @@
 import React, { Component} from 'react';
 import ReactDOM from 'react-dom';
-import logo from './logo.svg';
 import './App.css';
 import {FormGroup, Form, Col, FormControl, ControlLabel, Button, } from 'react-bootstrap';
 import {Link} from 'react-router';
