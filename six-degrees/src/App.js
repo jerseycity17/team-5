7fd75a91121dcbbb225234e52b2bfbb6a45a0b18
yaoc1996 <yaoc1996@gmail.com>
2017-10-27 23:34:53 -0400

    added routing

--- six-degrees/src/App.js
+++ six-degrees/src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route , Switch, Link} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      signed_in: false,
+      userId: null,
+    };
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Switch>
+        //insert routes
+      </Switch>
     );
   }
 }
