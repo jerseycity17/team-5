28a2ffae93f8d5156511659abfde3fffa034fa3e
Yuan Zhou <yuanzh10@yahoo.com>
2017-10-28 01:31:33 -0400

    added landing page, and navigation bar component

--- six-degrees/src/App.js
+++ six-degrees/src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route , Switch, Link} from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom';
+import {Navbar, Nav, NavItem, NavDropdown, MenuItem, } from 'react-bootstrap';
+import NavbarInstance from "./NavbarInstance.js";
+import CarouselInstance from "./CarouselInstance";
 
 class App extends Component {
   constructor() {
@@ -13,9 +16,21 @@ class App extends Component {
   }
   render() {
     return (
+      <div className="App">
+      <NavbarInstance/>
+        <header className="App-header">
+          <img src="https://pbs.twimg.com/profile_images/907642405683290114/E2FU6vic.jpg"className="App-logo"/>
+          <h1 className="App-title"></h1>
+        </header>
+        <p className="App-intro">
+          <CarouselInstance/>
+        </p>
+     
+
       <Switch>
         //insert routes
-      </Switch>
+      </Switch> 
+      </div>
     );
   }
 }
